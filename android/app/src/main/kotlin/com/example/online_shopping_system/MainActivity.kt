e503cb405b9c646be1cb9e6b5eb3d54ec6db29bd
Menna-Ashraf-S <110237289+Menna-Ashraf-S@users.noreply.github.com>
2022-11-07 23:13:36 +0200

    template

--- android/app/src/main/kotlin/com/example/online_shopping_system/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.online_shopping_system
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,42 @@
 import 'package:flutter/material.dart';
-import 'package:online_shopping_system/pages/navigation.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home:  Navigation(),
+      debugShowCheckedModeBanner: false ,
+      home: Home(),
     );
   }
 }
+
+class Home  extends StatefulWidget {
+ 
+  @override
+  _HomeState  createState() => _HomeState();
+  
+}
+
+
+class _HomeState extends State < Home > {
+
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      body: null 
+    );
+
+  }
+  
+  }
+  
+
+  
+	
