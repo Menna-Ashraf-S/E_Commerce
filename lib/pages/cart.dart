fb31ebc583fedc7693ce9ff0774d7ce976ceca42
Mohamed Elweza <moahmed9899@gmail.com>
2022-11-12 03:30:45 +0200

    Finish CartScreen

--- lib/pages/cart.dart
+++ lib/pages/cart.dart
@@ -4,6 +4,8 @@ import 'package:flutter_pro/local/cart_SQL.dart';
 import 'package:flutter_pro/local/dbHelper_cart.dart';
 import 'package:flutter_pro/pages/products.dart';
 
+import '../local/navigat_details.dart';
+
 class CartScreen extends StatefulWidget {
 
   CartScreen({Key? key}) : super(key: key);
@@ -52,146 +54,192 @@ class _CartScreenState extends State<CartScreen> {
                         itemCount: carts.length,
                         itemBuilder: (context, index) {
                           return SingleChildScrollView(
-                            child: Card(
-
-                              child: ListTile(
-                                leading: SizedBox(
-                                    // height: 100,
-                                    width: 70,
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(8.0),
-                                      child: Image.network(
-                                        carts.elementAt(index).image,
-                                        fit: BoxFit.cover,
-                                        height: 200,
-                                      ),
-                                    )),
-                                title:  Padding(
-                                  padding: EdgeInsets.all(
-                                    10.0,
+                            child: GestureDetector(
+                              onTap: () async {
+                                if (carts[index].category == 'electronics') {
+                                  Navigator.pushNamed(context, '/detailsWO',
+                                      arguments: Navigat_Details(
+                                          image: carts[index].image.toString(),
+                                          title: carts[index].title,
+                                          price: carts[index].price,
+                                          description: carts[index].description.toString(),
+                                          rating: double.parse(carts[index].rating.toString()),
+                                          count: carts[index].count,
+                                          category: carts[index].category));
+                                } else if (carts[index].category == 'jewelery') {
+                                  Navigator.pushNamed(context, '/detailsWS',
+                                      arguments: Navigat_Details(
+                                          image: carts[index].image.toString(),
+                                          title: carts[index].title,
+                                          price: carts[index].price,
+                                          description: carts[index].description.toString(),
+                                          rating: double.parse(carts[index].rating.toString()),
+                                          count: carts[index].count,
+                                          category: carts[index].category));
+                                } else {
+                                  Navigator.pushNamed(context, '/details',
+                                      arguments: Navigat_Details(
+                                          image: carts[index].image.toString(),
+                                          title: carts[index].title,
+                                          price: carts[index].price,
+                                          description: carts[index].description.toString(),
+                                          rating: double.parse(carts[index].rating.toString()),
+                                          count: carts[index].count,
+                                          category: carts[index].category));
+                                }
+                                setState(() {});
+                              },
+                              child: Card(
+                                child: ListTile(
+                                  leading: SizedBox(
+                                      // height: 100,
+                                      width: 70,
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(8.0),
+                                        child: Image.network(
+                                          carts.elementAt(index).image,
+                                          fit: BoxFit.cover,
+                                          height: 200,
+                                        ),
+                                      )),
+                                  title:  Padding(
+                                    padding: EdgeInsets.all(
+                                      10.0,
+                                    ),
+                                    child: Text(
+                                      carts.elementAt(index).title,
+                                      maxLines: 2,
+                                      style: TextStyle(
+                                          overflow: TextOverflow.ellipsis,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20),
+                                    ),
                                   ),
-                                  child: Text(
-                                    carts.elementAt(index).title,
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 25),
-                                  ),
-                                ),
-                                subtitle: Padding(
-                                  padding: const EdgeInsets.all(
-                                    8.0,
-                                  ),
-                                  child: Column(
-                                    children: [
-                                      Row(
-                                        children: [
-                                          Text(
-                                            carts.elementAt(index).price.toString(),
-                                            style: TextStyle(
-                                              fontSize: 20,
-                                              color: Colors.black,
+                                  subtitle: Padding(
+                                    padding: const EdgeInsets.all(
+                                      8.0,
+                                    ),
+                                    child: Column(
+                                      children: [
+                                        Row(
+                                          children: [
+                                            Text(
+                                              '${carts.elementAt(index).price.toString()}\$',
+                                              style: TextStyle(
+                                                fontSize: 20,
+                                                color: Colors.black,fontWeight: FontWeight.bold
+                                              ),
                                             ),
-                                          ),
-                                        ],
+                                          ],
+                                        ),
+                                        // Row(
+                                        //   children: [
+                                        //     const Text(
+                                        //       'QTY:',
+                                        //       style: TextStyle(
+                                        //           fontSize: 20,
+                                        //           fontWeight: FontWeight.bold),
+                                        //     ),
+                                        //     Text(
+                                        //       '$num',
+                                        //       style: const TextStyle(
+                                        //           fontSize: 20,
+                                        //           color: Colors.black,
+                                        //           fontWeight: FontWeight.bold),
+                                        //     )
+                                        //   ],
+                                        // ),
+                                      ],
+                                    ),
+                                  ),
+                                  trailing: Column(
+                                    children: [
+                                      const SizedBox(
+                                        height: 3,
                                       ),
-                                      Row(
-                                        children: [
-                                          const Text(
-                                            'QTY:',
-                                            style: TextStyle(
-                                                fontSize: 20,
-                                                fontWeight: FontWeight.bold),
-                                          ),
-                                          Text(
-                                            '$num',
-                                            style: const TextStyle(
-                                                fontSize: 20,
-                                                color: Colors.black,
-                                                fontWeight: FontWeight.bold),
-                                          )
-                                        ],
-                                      ),
+                                      // ClipRRect(
+                                      //   borderRadius: BorderRadius.circular(8.0),
+                                      //   child: Container(
+                                      //     height: 25,
+                                      //     width: 30,
+                                      //     color: Colors.black12,
+                                      //     child:
+                                      //     IconButton(
+                                      //       onPressed: () {
+                                      //         setState(() {
+                                      //           num++;
+                                      //         });
+                                      //       },
+                                      //       color: Colors.white,
+                                      //       icon: const Icon(
+                                      //         Icons.add,
+                                      //         color: Colors.black,
+                                      //         size: 12.5,
+                                      //       ),
+                                      //     ),
+                                      //     IconButton(onPressed: (){
+                                      //       setState(() {
+                                      //         CartHelper.instance
+                                      //             .deleteCart(carts[index].id!);
+                                      //       });
+                                      //       }, icon: Icon(Icons.delete),),
+                                      //
+                                      //   ),
+                                      // ),
+                                      // const SizedBox(
+                                      //   height: 3,
+                                      // ),
+                                      // ClipRRect(
+                                      //   borderRadius: BorderRadius.circular(8.0),
+                                      //   child: Container(
+                                      //     height: 25,
+                                      //     width: 30,
+                                      //     color: Colors.black12,
+                                      //     child: IconButton(
+                                      //       onPressed: () {
+                                      //         setState(() {
+                                      //           num == 1 ? num = 1 : num--;
+                                      //         });
+                                      //       },
+                                      //       icon: const Icon(
+                                      //         Icons.remove,
+                                      //         color: Colors.black,
+                                      //         size: 12.5,
+                                      //       ),
+                                      //     ),
+                                      //   ),
+                                      // ),
+                                      // SizedBox(height: 15,),
+                                      IconButton(onPressed: (){
+                                        setState(() {
+                                          CartHelper.instance
+                                              .deleteCart(carts[index].id!);
+                                        });
+                                      }, icon: Icon(Icons.delete, size: 30, color: Colors.red,),),
                                     ],
                                   ),
-                                ),
-                                trailing: Column(
-                                  children: [
-                                    const SizedBox(
-                                      height: 3,
-                                    ),
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(8.0),
-                                      child: Container(
-                                        height: 25,
-                                        width: 30,
-                                        color: Colors.black12,
-                                        child:
-                                        // IconButton(
-                                        //   onPressed: () {
-                                        //     setState(() {
-                                        //       num++;
-                                        //     });
-                                        //   },
-                                        //   color: Colors.white,
-                                        //   icon: const Icon(
-                                        //     Icons.add,
-                                        //     color: Colors.black,
-                                        //     size: 12.5,
-                                        //   ),
-                                        // ),
-                                        IconButton(onPressed: (){
-                                          setState(() {
-                                            CartHelper.instance
-                                                .deleteCart(carts[index].id!);
-                                          });
-                                          }, icon: Icon(Icons.delete),),
-
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      height: 3,
-                                    ),
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(8.0),
-                                      child: Container(
-                                        height: 25,
-                                        width: 30,
-                                        color: Colors.black12,
-                                        child: IconButton(
-                                          onPressed: () {
-                                            setState(() {
-                                              num == 1 ? num = 1 : num--;
-                                            });
-                                          },
-                                          icon: const Icon(
-                                            Icons.remove,
-                                            color: Colors.black,
-                                            size: 12.5,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                    // SizedBox(height: 15,),
-
-                                  ],
                                 ),
                               ),
                             ),
                           );
                         }),
                   ),
-                  ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        shape: new RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(30.0),
+                  Container(
+                    height: 50,
+                    width: 160,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          shape: new RoundedRectangleBorder(
+                            borderRadius: new BorderRadius.circular(30.0),
+                          ),
+                          backgroundColor: Colors.black,
                         ),
-                        backgroundColor: Colors.black,
-                      ),
-                      child: Text(
-                        'Checkout',
-                        style: TextStyle(color: Colors.white),
-                      ))
+                        child: Text(
+                          'Checkout',
+                          style: TextStyle(color: Colors.white, fontSize: 25),
+                        )),
+                  )
                 ],
               );
             }
